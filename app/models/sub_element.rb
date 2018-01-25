d804f8efaa9ec20421ce9428ffaa74faf0e06a70
Eon <eoneon123@gmail.com>
2018-01-25 15:28:19 -0800

    added navigation links

--- app/models/sub_element.rb
+++ app/models/sub_element.rb
@@ -1,4 +1,6 @@
 class SubElement < ApplicationRecord
+  include Importable
+  
   has_many :element_groups, dependent: :destroy
   has_many :edition_types, through: :element_groups, source: :elementable, source_type: 'Element'
   has_many :medium_types, through: :element_groups, source: :elementable, source_type: 'Element'
@@ -10,32 +12,32 @@ class SubElement < ApplicationRecord
 
   validates :name, presence: true
 
-  def self.to_csv(options = {})
-    CSV.generate(options) do |csv|
-      csv << column_names
-      all.each do |sub_element|
-        csv << sub_element.attributes.values_at(*column_names)
-      end
-    end
-  end
-
-  def self.import(file)
-    spreadsheet = Roo::Spreadsheet.open(file.path)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
-      sub_element = find_by(id: row["id"]) || new
-      sub_element.attributes = row.to_hash
-      sub_element.save!
-    end
-  end
-
-  def self.open_spreadsheet(file)
-    case File.extname(file.original_filename)
-    when ".csv" then Csv.new(file.path, nil, :ignore)
-    when ".xls" then Excel.new(file.path, nil, :ignore)
-    when ".xlsx" then Excelx.new(file.path, nil, :ignore)
-    else raise "Unknown file type: #{file.original_filename}"
-    end
-  end
+  # def self.to_csv(options = {})
+  #   CSV.generate(options) do |csv|
+  #     csv << column_names
+  #     all.each do |sub_element|
+  #       csv << sub_element.attributes.values_at(*column_names)
+  #     end
+  #   end
+  # end
+  #
+  # def self.import(file)
+  #   spreadsheet = Roo::Spreadsheet.open(file.path)
+  #   header = spreadsheet.row(1)
+  #   (2..spreadsheet.last_row).each do |i|
+  #     row = Hash[[header, spreadsheet.row(i)].transpose]
+  #     sub_element = find_by(id: row["id"]) || new
+  #     sub_element.attributes = row.to_hash
+  #     sub_element.save!
+  #   end
+  # end
+  #
+  # def self.open_spreadsheet(file)
+  #   case File.extname(file.original_filename)
+  #   when ".csv" then Csv.new(file.path, nil, :ignore)
+  #   when ".xls" then Excel.new(file.path, nil, :ignore)
+  #   when ".xlsx" then Excelx.new(file.path, nil, :ignore)
+  #   else raise "Unknown file type: #{file.original_filename}"
+  #   end
+  # end
 end
